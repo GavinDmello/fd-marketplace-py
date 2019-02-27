d79d09cc64ec2b7554f7a0426f658832eb836fef
Gavin Dmello <dmellogavin5000@gmail.com>
2019-02-27 00:27:30 +0000

    First commit: Got auth in place for other APIs

---
+++ auth.py
@@ -0,0 +1,31 @@
+from hashlib import sha1
+import email.utils
+import hmac
+import requests
+
+class Auth(object):
+	url = None
+	secret = None
+	username = None
+	
+	@staticmethod
+	def set_credentials(username, secret, url):
+		Auth.username = username
+		Auth.secret = secret
+		Auth.url = url
+
+	@staticmethod
+	def get_headers():
+		dt = email.utils.formatdate(usegmt=True)
+		stringToSign = 'date: ' + dt;
+
+		encodedSignature = hmac.new(Auth.secret, stringToSign, sha1).digest().encode("base64").rstrip('\n')
+
+		hmacAuth = 'hmac username="' + Auth.username + '",algorithm="hmac-sha1",headers="date",signature="' + encodedSignature + '"';
+
+		headers = {
+    		'date': dt,
+    		'Authorization': hmacAuth
+		}
+
+		return headers
