c20662001d6635a46e9d2d84e52e1e5b972098fb
Gavin Dmello <dmellogavin5000@gmail.com>
2019-05-05 00:41:51 +0100

    Added industry API

---
+++ api/industry.py
@@ -0,0 +1,35 @@
+import requests
+from auth import Auth
+
+class Industry(object):
+	def __init__(self):
+		self.url = Auth.url
+		self.headers = Auth.get_headers()
+
+	'''
+	This endpoint retrieves the broad industry description (industryDescription) 
+	labels for a given business category. Result values returned by this endpoint 
+	can be used in the request of the GET MCC/Industry endpoint.
+	'''
+	def get_industry(self, category_name):
+		endpoint = url + "/marketplace/v1/categories/{}/industries"
+		endpoint = endpoint.format(category_name)
+
+		return self.make_get_request(endpoint, self.headers)
+
+	'''
+	This endpoint retrieves the MCC - Merchant Category Code(s) - for a given industry.
+
+	'''
+	def get_mcc_indusrty(self, category_name, industry_description):
+		endpoint = url + "/marketplace/v1/categories/{}/industries/{}/merchantcategorycodes"
+		endpoint = endpoint.format(category_name, industry_description)
+
+		return self.make_get_request(endpoint, self.headers)
+
+	def make_get_request(self, url, headers):
+		r = requests.get(url, headers=headers)
+		if r.status_code == 200:
+			return r.content
+		else:
+			raise Exception("Exception, API returned " + str(r.status_code))
